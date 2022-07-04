b56e27e68ec2415fc0703d0ea5942abfbdb90f65
Adilson <adilsonvirgo@gmail.com>
2022-07-04 02:39:45 -0500

    Photography model

---
+++ app/Http/Controllers/PhotographyController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Photography;
+use App\Http\Requests\StorePhotographyRequest;
+use App\Http\Requests\UpdatePhotographyRequest;
+
+class PhotographyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StorePhotographyRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StorePhotographyRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Photography  $photography
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Photography $photography)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Photography  $photography
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Photography $photography)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdatePhotographyRequest  $request
+     * @param  \App\Models\Photography  $photography
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdatePhotographyRequest $request, Photography $photography)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Photography  $photography
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Photography $photography)
+    {
+        //
+    }
+}
